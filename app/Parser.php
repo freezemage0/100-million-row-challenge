3512d9923f156865021fdea0b018ae4dce81f695
Freezemage <freezemage0@gmail.com>
2026-02-26 00:59:40 +0300

    WIP.

--- app/Parser.php
+++ app/Parser.php
@@ -10,20 +10,13 @@ final class Parser
 
         $file = \fopen($inputPath, 'rb');
         while ($line = \fgetcsv($file, escape: '"')) {
-            $date = \substr($line[1], 0, 10);
-
-            $path = \parse_url($line[0], \PHP_URL_PATH);
-
-            $result[$path] ??= [];
-            $result[$path][] = $date;
+            $result[\parse_url($line[0], \PHP_URL_PATH)][] = \substr($line[1], 0, 10);
         }
-        \fclose($file);
 
         foreach ($result as &$visits) {
-            \sort($visits);
             $visits = \array_count_values($visits);
+            \ksort($visits);
         }
-        unset($visits);
 
         \file_put_contents($outputPath, \json_encode($result, \JSON_PRETTY_PRINT));
     }
