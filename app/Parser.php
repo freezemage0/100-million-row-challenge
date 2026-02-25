c92c1ba6d86e5e6b9b6aec5d615faa4343690f6b
Freezemage <freezemage0@gmail.com>
2026-02-26 00:23:07 +0300

    WIP.

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,29 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $result = [];
+
+        $file = \fopen($inputPath, 'rb');
+        while ($line = \fgetcsv($file, escape: '"')) {
+            $date = \substr($line[1], 0, 10);
+
+            $path = \parse_url($line[0], \PHP_URL_PATH);
+
+            $result[$path] ??= [];
+            $result[$path][] = $date;
+        }
+        \fclose($file);
+
+        foreach ($result as &$visits) {
+            \sort($visits);
+            $visits = \array_count_values($visits);
+        }
+        unset($visits);
+
+        \file_put_contents($outputPath, \json_encode($result, \JSON_PRETTY_PRINT));
     }
 }
